3ab4b85cc22eb106d226d01ccd40a7a10702b1bd
Nikiyirou <li.6744@buckeyemail.osu.edu>
2021-04-26 21:23:16 -0700

    update HW

---
+++ src/Collection/DataBase.java
@@ -0,0 +1,41 @@
+package Collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataBase {
+    private final Map<String, Employee> map = new HashMap<>();
+
+    public void create(Employee record){
+        map.put(record.id, record);
+    }
+
+    public Employee read(String id) {
+        return map.get(id);
+    }
+
+    public void update(Employee record){
+        map.put(record.id, record);
+    }
+
+    public void delete(Employee record){
+        map.remove(record.id);
+    }
+
+    public void delete(String id){
+        map.remove(id);
+    }
+}
+
+class Employee{
+    String id;
+    String firstName;
+    String lastName;
+    String birthDay;
+    public void Employee(String id, String firstName ,String lastName ,String birthDay) {
+        this.id = id;
+        this. firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+    }
+}
